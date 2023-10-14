49d3ce3d7c10b4ed54bd558dfa935265abe29aff
aSwanting <gab.damico@gmail.com>
2023-10-14 13:45:36 +0200

    add: Bouncing circle v2

---
+++ js/script.js
@@ -0,0 +1,79 @@
+"use strict"
+
+// DOM Variables, create testBox and testCircle
+const appBody = document.querySelector(".app-body")
+const testBox = document.createElement("div")
+const testCircle = document.createElement("div")
+appBody.append(testBox)
+testBox.append(testCircle)
+testBox.className = "test-box"
+testCircle.className = "test-circle"
+
+// Get circle radius
+const circleR = testCircle.offsetWidth / 2
+
+// Initialize boundaries
+let leftBound = circleR
+let topBound = circleR
+let rightBound = testBox.offsetWidth - circleR
+let bottomBound = testBox.offsetHeight - circleR
+
+// Update boundaries on window resize
+window.onresize = function () {
+    rightBound = testBox.offsetWidth - circleR
+    bottomBound = testBox.offsetHeight - circleR
+}
+
+// Randomize circle spawn location within boundaries
+let circleX = rand(rightBound, leftBound)
+let circleY = rand(topBound, bottomBound)
+testCircle.style.left = circleX + "px"
+testCircle.style.top = circleY + "px"
+
+// Randomize circle direction and speed
+let circleLtoR = rand(0, 1)
+let circleTtoB = rand(0, 1)
+const speedX = rand(5, 15)
+const speedY = rand(5, 15)
+
+// Timing variables
+const fps = 60
+const frameInterval = 1000 / fps
+let previouTime = performance.now()
+let delaTimeMultiplier = 1
+let deltaTime = 0
+
+// Animation Function
+function moveCircle(currentTime) {
+
+    // Calculate DeltaTime and DeltaTimeMultiplier
+    deltaTime = currentTime - previouTime
+    delaTimeMultiplier = deltaTime / frameInterval
+
+    // Give circle horizontal direction and speed
+    circleX < rightBound && circleLtoR ? circleX += speedX * delaTimeMultiplier : circleLtoR = false
+    circleX > leftBound && !circleLtoR ? circleX -= speedX * delaTimeMultiplier : circleLtoR = true
+
+    // Give circle vertical direction and speed
+    circleY < bottomBound && circleTtoB ? circleY += speedY * delaTimeMultiplier : circleTtoB = false
+    circleY > topBound && !circleTtoB ? circleY -= speedY * delaTimeMultiplier : circleTtoB = true
+
+    // Update animation time
+    previouTime = currentTime
+
+    // Move circle
+    testCircle.style.left = circleX + "px"
+    testCircle.style.top = circleY + "px"
+
+    // Request next frame
+    requestAnimationFrame(moveCircle)
+}
+
+// Run animation
+requestAnimationFrame(moveCircle)
+
+
+// Math Random Function
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
